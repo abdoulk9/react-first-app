76723e7fdafb32ab844c14d8d13f5ab5e27a84a1
abdoulk9 <abdoulkadre@yahoo.fr>
2019-04-30 15:34:29 +0200

    Updates

---
+++ precache-manifest.91631e68e77a6e26d9da15ad6e7867ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9867f6f4625fd4708bc0e5a63b60f81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f7d2dba1fdc82cee2f",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "c4029acf1ebd9358cea7",
+    "url": "/static/js/2.a3fd9682.chunk.js"
+  },
+  {
+    "revision": "c1f7d2dba1fdc82cee2f",
+    "url": "/static/js/main.28532da5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
